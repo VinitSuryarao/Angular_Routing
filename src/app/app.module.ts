594d41197a884444c26f676d2d83ede34f3315b3
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-09-26 19:09:18 +0530

    app init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes : Routes =[
+  {path:"home", component :HomeComponent},
+  {path:"about-us", component :AboutUsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
+    
   ],
+
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
